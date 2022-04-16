2924e5d46c8ba6508f3e87673e9622883bc70bb9
Neuton Foo <neuton.foo@gmail.com>
2022-04-16 15:34:10 -0700

    Streaks pt1

--- src/modules/wordle.model.ts
+++ src/modules/wordle.model.ts
@@ -3,19 +3,26 @@ import { model, Schema } from "mongoose";
 export interface IPlayer {
   id: number;
   name: string;
-  totalScore: number;
-  games: [
-    {
-      wordleIndex: number;
-      isHardMode: boolean;
-      attempts: number;
-    }
-  ];
+  pointsScore: number;
+  roundsScore: number;
+  longestStreak: number;
+  currentStreak: number;
+  isStreaking: boolean;
+  games: Game[];
 }
 
-export const playerSchema = new Schema<IPlayer>({
+export interface Game {
+  wordleIndex: number;
+  isHardMode: boolean;
+  attempts: number;
+}
+
+const playerSchema = new Schema<IPlayer>({
   id: { type: Number, required: true },
   name: { type: String, required: true },
+  pointsScore: { type: Number },
+  roundsScore: { type: Number },
+  longestStreak: { type: Number },
   games: [
     {
       wordleIndex: Number,
@@ -26,3 +33,29 @@ export const playerSchema = new Schema<IPlayer>({
 });
 
 export const Player = model<IPlayer>("Player", playerSchema);
+
+export interface IWordleMeta {
+  currentWordleIndex: number;
+  numberOfPlayers: number;
+  numberOfPlays: number;
+  streaks: {
+    id: number;
+    name: string;
+    games: number;
+  }[];
+}
+
+const wordleMetaSchema = new Schema<IWordleMeta>({
+  currentWordleIndex: { type: Number },
+  numberOfPlayers: { type: Number },
+  numberOfPlays: { type: Number },
+  streaks: [
+    {
+      id: { type: Number },
+      name: { type: String },
+      games: { type: Number },
+    },
+  ],
+});
+
+export const WordleMeta = model<IWordleMeta>("WordleMeta", wordleMetaSchema);
--- src/modules/wordle.ts
+++ src/modules/wordle.ts
@@ -1,10 +1,8 @@
 import { Client, Message, TextChannel } from "discord.js";
 import { connect } from "mongoose";
-import { IPlayer, Player } from "./wordle.model";
+import { Game, IPlayer, Player, WordleMeta } from "./wordle.model";
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
-// First Wordle = 19 June 2021;
 
 // # Command Prefix
 const prefix = "!w ";
@@ -13,7 +11,6 @@ const prefix = "!w ";
 const WORDLE_CHANNEL_ID = process.env.WORDLE_CHANNEL_ID;
 
 // # MongoDB Connection String
-let hasConnectedToMongoDB = false;
 const MONGODB_CONNECTION = `${process.env.MONGODB_BASE_CONNECTION}wordleTracker?retryWrites=true&w=majority`;
 
 // # Scoring
@@ -21,13 +18,13 @@ const MONGODB_CONNECTION = `${process.env.MONGODB_BASE_CONNECTION}wordleTracker?
 // ## Extra points to award when played in hard mode (excluding failures)
 const scoringHardMode = 1;
 const scoring: number[] = [
-  0, // 0 Attempts / Fail
   6, // 1 Attempt
   5, // 2 Attempts
   4, // 3 Attempts
   3, // 4 Attempts
   2, // 5 Attempts
   1, // 6 Attempts
+  0, // 0 Attempts / Fail
 ];
 
 // # Wordle result match Regex
@@ -52,15 +49,20 @@ module.exports = {
     {
       name: "ready",
       async execute(client: Client) {
-        // const channel = client.channels.cache.get(
-        //   WORDLE_CHANNEL_ID!
-        // ) as TextChannel;
-        // for (const twiceEmote of twiceReactionEmojiIds) {
-        //   const someEmoji = client.emojis.cache.get(twiceEmote);
-        //   if (someEmoji) {
-        //     channel.send(someEmoji.toString());
-        //   }
-        // }
+        await connect(MONGODB_CONNECTION);
+
+        // ###
+        // ### IMPORTANT: THIS IS ONLY RUN ONCE TO GENERATE INITIAL CONFIG FILE
+        // ###
+
+        // const wordleMeta = new WordleMeta({
+        //   currentWordleIndex: 297,
+        //   numberOfPlayers: 4,
+        //   numberOfPlays: 0,
+        //   streaks: [],
+        // });
+        // await wordleMeta.save();
+        // (await cronGameChecker(client)).start();
       },
     },
     {
@@ -68,16 +70,11 @@ module.exports = {
       async execute(message: Message) {
         if (message.author.bot) return false;
 
-        if (!hasConnectedToMongoDB) {
-          await connect(MONGODB_CONNECTION);
-          hasConnectedToMongoDB = true;
-        }
-
         // Get message info
         const content = message.content;
         const channelId = message.channelId;
         const authorId = message.author.id;
-        const authorName = message.author.username;
+        // const authorName = message.author.username;
 
         // Global commands
         if (content.startsWith(prefix)) {
@@ -89,176 +86,45 @@ module.exports = {
             commandParts[0] === "l" ||
             commandParts[0] === "leaderboard"
           ) {
-            // # Leaderboard
-            const playersClean: IPlayer[] = [];
-
-            const players = await Player.find();
-            for (const player of players) {
-              let totalScore = 0;
-
-              for (const game of player.games) {
-                // If attempts > 0 -> they did not fail
-                if (game.attempts > 0) {
-                  totalScore += scoring[game.attempts];
-
-                  if (game.isHardMode) totalScore += scoringHardMode;
-                }
-              }
-
-              playersClean.push({
-                id: player.id,
-                name: player.name,
-                totalScore: totalScore,
-                games: player.games,
-              });
-            }
-
-            // Sort by totalScore descending
-            playersClean.sort(
-              (player1, player2) => player2.totalScore - player1.totalScore
-            );
-
-            if (playersClean.length === 0) {
-              await message.channel.send("No players yet.");
-            } else {
-              await message.channel.send(
-                "**Wordle Leaderboard**\n" +
-                  playersClean
-                    .map(
-                      (player, playerIndex) =>
-                        `${playerIndex + 1}: ${player.name} has **${
-                          player.totalScore
-                        } point${player.totalScore === 1 ? "" : "s"}** over ${
-                          player.games.length
-                        } game${player.games.length === 1 ? "" : "s"}.`
-                    )
-                    .join("\n")
-              );
-            }
+            leaderboard(message.channel as TextChannel);
           } else if (commandParts[0] === "s" || commandParts[0] === "stats") {
-          } else if (commandParts[0] === "bowser_revolution") {
-            await message.channel.send(
-              "I've been thinking. Everybody's so focused on :slot_machine: points. But there's more to life..."
-            );
-            await message.channel.send(
-              "Maybe you'll understand if you're all equal... with the same number of :slot_machine: points!"
-            );
+          }
 
-            const playersClean: IPlayer[] = [];
-
-            let totalPoints = 0;
-
-            const players = await Player.find();
-            for (const player of players) {
-              for (const game of player.games) {
-                // If attempts > 0 -> they did not fail
-                if (game.attempts > 0) {
-                  totalPoints += scoring[game.attempts];
-                  if (game.isHardMode) totalPoints += scoringHardMode;
-                }
-              }
-
-              playersClean.push({
-                id: player.id,
-                name: player.name,
-                totalScore: 0,
-                games: player.games,
-              });
+          // # Admin Commands
+          if (authorId === process.env.ADMIN_DISCORD_ID) {
+            // Validate
+            if (commandParts[0] === "recalculate_points") {
+              adminRecalculatePoints();
+              leaderboard(message.channel as TextChannel);
             }
-
-            for (const player of playersClean) {
-              player.totalScore = totalPoints / playersClean.length;
-            }
-
-            await message.channel.send(
-              `Total points: **${totalPoints}**. Points per player = ${totalPoints} / ${
-                playersClean.length
-              } = **${totalPoints / playersClean.length}**`
-            );
-
-            await message.channel.send(
-              "**Wordle Leaderboard**\n" +
-                playersClean
-                  .map(
-                    (player, playerIndex) =>
-                      `${playerIndex + 1}: ${player.name} has **${
-                        player.totalScore
-                      } point${player.totalScore === 1 ? "" : "s"}** over ${
-                        player.games.length
-                      } game${player.games.length === 1 ? "" : "s"}.`
-                  )
-                  .join("\n")
-            );
           }
         }
 
         // If in Wordle channel
         if (channelId === WORDLE_CHANNEL_ID) {
           const firstLine = content.split(/\r?\n/)[0];
-
-          const wordleResultMatches = firstLine.match(WORDLE_RESULT_REGEX);
-
-          // If Wordle
-          if (wordleResultMatches) {
-            // Parse Wordle
-            const wordleIndex = Number.parseInt(
-              wordleResultMatches.groups!.wordleIndex
-            );
-            const isHardMode = wordleResultMatches.groups!.isHardMode === "*";
-            const attempts = wordleResultMatches.groups!.attempts;
-
-            const player =
-              (await Player.findOne({ id: authorId })) ||
-              new Player({ id: authorId, name: authorName, games: [] });
-
-            if (!player.games.find(game => game.wordleIndex === wordleIndex)) {
-              player.games.push({
-                wordleIndex: wordleIndex,
-                isHardMode: isHardMode,
-                attempts: attempts === "X" ? 0 : Number.parseInt(attempts),
-              });
-
-              await player.save();
-
-              message.react(
-                twiceReactionEmojiIds[
-                  Math.floor(Math.random() * twiceReactionEmojiIds.length)
-                ]
-              );
-
-              let totalScore = 0;
-
-              for (const game of player.games) {
-                // If attempts > 0, or if they did not fail
-                if (game.attempts > 0) {
-                  totalScore += scoring[game.attempts];
-
-                  if (game.isHardMode) totalScore += scoringHardMode;
-                }
-              }
-
-              await message.channel.send(
-                `<@${authorId}> - Wordle game ${wordleIndex} added (+${
-                  attempts === "X"
-                    ? "0"
-                    : isHardMode
-                    ? scoring[Number.parseInt(attempts)] +
-                      scoringHardMode +
-                      `\\\*`
-                    : scoring[Number.parseInt(attempts)]
-                } points). You now have **${totalScore} points**.`
-              );
-            } else {
-              await message.channel.send(
-                `<@${authorId}> - Wordle game ${wordleIndex} already added.`
-              );
-            }
-          }
+          parseWordle(firstLine, message);
         }
       },
     },
   ],
 };
+
+// async function cronGameChecker(client: Client): Promise<CronJob> {
+//   return new CronJob(
+//     "*/10 * * * * *",
+//     // "59 23 * * *",
+//     function () {
+//       const channel = client.channels.cache.get(
+//         WORDLE_CHANNEL_ID!
+//       ) as TextChannel;
+//       channel.send(`test ${Math.random()}`);
+//     },
+//     null,
+//     false,
+//     "America/Los_Angeles"
+//   );
+// }
 
 async function help(channel: TextChannel) {
   let helpMessage = `**Wordle Tracker Help**\n`;
@@ -268,4 +134,221 @@ async function help(channel: TextChannel) {
   helpMessage += `\`${prefix}s @user\`: Show stats for a specific user`;
 
   await channel.send(helpMessage);
+}
+
+async function leaderboard(channel: TextChannel) {
+  // # Leaderboard
+  const players = await Player.find().sort({ pointsScore: -1 });
+
+  if (players.length === 0) {
+    await channel.send("No players yet.");
+  } else {
+    let currentRank = 1;
+    let playersInCurrentRank = 0;
+    let previousPointScore = 0;
+    let isFirstPlayer = true;
+
+    const wordleMeta = await WordleMeta.findOne();
+
+    await channel.send(
+      `**Wordle Leaderboard**\n` +
+        `(Streaks are currently not automatically recalculated)\n` +
+        wordleMeta!.streaks
+          .map(
+            player =>
+              `:fire: ${player.name} is on a streak of ${player.games} game${
+                player.games === 1 ? "" : "s"
+              } :fire: `
+          )
+          .join("\n") +
+        "\n\n" +
+        players
+          .map(player => {
+            if (player.pointsScore < previousPointScore || isFirstPlayer) {
+              currentRank = currentRank + playersInCurrentRank;
+              playersInCurrentRank = 1;
+
+              isFirstPlayer = false;
+            } else if (player.pointsScore === previousPointScore) {
+              playersInCurrentRank++;
+            }
+
+            previousPointScore = player.pointsScore;
+
+            return `${currentRank}: ${player.name} has **${
+              player.pointsScore
+            } point${player.pointsScore === 1 ? "" : "s"}** over ${
+              player.games.length
+            } game${player.games.length === 1 ? "" : "s"}.`;
+          })
+          .join("\n")
+    );
+  }
+}
+
+async function adminRecalculatePoints() {
+  const players = await Player.find();
+
+  const playersCopy: IPlayer[] = JSON.parse(JSON.stringify(players));
+
+  // Calculate pointsScore
+  for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+    const player = players[playerIndex];
+
+    // Sort player.games by Wordle Index desc for later
+    playersCopy[playerIndex].games.sort(
+      (game1, game2) => game2.wordleIndex - game1.wordleIndex
+    );
+
+    let pointsScore = 0;
+
+    for (const game of player.games) {
+      pointsScore += scoring[game.attempts - 1];
+      if (game.isHardMode) pointsScore += scoringHardMode;
+    }
+
+    player.pointsScore = pointsScore;
+    player.save();
+  }
+
+  // # Calculate roundsScore
+  // Games already sorted by Wordle Index Desc
+
+  const longestPlayerGames = Math.max(
+    ...playersCopy.map(({ games }) => games.length)
+  );
+
+  const latestMinAttempts = Math.min(
+    ...playersCopy.map(({ games }) => games[0].attempts)
+  );
+  const latestStreakPlayers = playersCopy.filter(
+    player => player.games[0].attempts === latestMinAttempts
+  );
+  latestStreakPlayers.forEach(player => {
+    player.currentStreak = 1;
+    player.isStreaking = true;
+  });
+
+  // Go down each game and add streaks
+  for (let gameIndex = 1; gameIndex < longestPlayerGames; gameIndex++) {
+    const minAttempts = Math.min(
+      ...playersCopy.map(({ games }) =>
+        games[gameIndex] ? games[gameIndex].attempts : 7
+      )
+    );
+
+    const streakPlayers = latestStreakPlayers.filter(player =>
+      player.games[gameIndex]
+        ? player.games[gameIndex].attempts === minAttempts
+          ? player.isStreaking
+          : (player.isStreaking = false && false)
+        : (player.isStreaking = false && false)
+    );
+
+    for (const player of streakPlayers) {
+      player.currentStreak++;
+    }
+
+    if (latestStreakPlayers.filter(player => player.isStreaking).length === 0) {
+      break;
+    }
+  }
+  const wordleMeta = await WordleMeta.findOne();
+
+  if (wordleMeta) {
+    // for(const player of latestStreakPlayers) {
+    wordleMeta.streaks = latestStreakPlayers.map(player => ({
+      id: player.id,
+      name: player.name,
+      games: player.currentStreak,
+    }));
+    wordleMeta.save();
+  }
+}
+
+async function parseWordle(firstLine: string, message: Message) {
+  const authorId = message.author.id;
+  const authorName = message.author.username;
+
+  const wordleResultMatches = firstLine.match(WORDLE_RESULT_REGEX);
+
+  // If Wordle
+  if (wordleResultMatches) {
+    // Parse Wordle
+    const wordleIndex = Number.parseInt(
+      wordleResultMatches.groups!.wordleIndex
+    );
+    const isHardMode = wordleResultMatches.groups!.isHardMode === "*";
+    const attempts = wordleResultMatches.groups!.attempts;
+
+    const player =
+      (await Player.findOne({ id: authorId })) ||
+      new Player({ id: authorId, name: authorName, games: [] });
+
+    if (!player.games.find(game => game.wordleIndex === wordleIndex)) {
+      const game: Game = {
+        wordleIndex: wordleIndex,
+        isHardMode: isHardMode,
+        attempts: attempts === "X" ? 7 : Number.parseInt(attempts),
+      };
+
+      const deltaPointsScore =
+        scoring[game.attempts - 1] + (game.isHardMode ? scoringHardMode : 0);
+
+      player.pointsScore += deltaPointsScore;
+
+      player.games.push(game);
+
+      await player.save();
+
+      message.react(
+        twiceReactionEmojiIds[
+          Math.floor(Math.random() * twiceReactionEmojiIds.length)
+        ]
+      );
+
+      await message.channel.send(
+        `<@${authorId}> - Wordle game ${wordleIndex} added (+${deltaPointsScore} points). You now have **${player.pointsScore} points**.`
+      );
+    } else {
+      await message.channel.send(
+        `<@${authorId}> - Wordle game ${wordleIndex} already added.`
+      );
+
+      const wordleMeta = await WordleMeta.findOne();
+      if (wordleMeta) {
+        wordleMeta.numberOfPlays++;
+        wordleMeta.save();
+
+        if (wordleMeta.numberOfPlays === wordleMeta.numberOfPlayers) {
+          checkRounds(wordleMeta.currentWordleIndex);
+        }
+      }
+    }
+  }
+}
+
+async function checkRounds(currentWordleIndex: number) {
+  // Check Rounds
+  const players = await Player.find();
+
+  // const currentGames =
+
+  // const minAttempts = Math.min(
+  //   ...players.map(({ games }) =>
+  //     games[gameIndex] ? games[gameIndex].attempts : 7
+  //   )
+  // );
+
+  // const streakPlayers = latestStreakPlayers.filter(player =>
+  //   player.games[gameIndex]
+  //     ? player.games[gameIndex].attempts === minAttempts
+  //       ? player.isStreaking
+  //       : (player.isStreaking = false && false)
+  //     : (player.isStreaking = false && false)
+  // );
+
+  // for (const player of streakPlayers) {
+  //   player.currentStreak++;
+  // }
 }
