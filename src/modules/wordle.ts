67930235f2ef188931d64d692e1429c8037deb11
Neuton Foo <neuton.foo@gmail.com>
2022-06-30 15:20:33 -0700

    Removed special edge case

--- src/modules/wordle.ts
+++ src/modules/wordle.ts
@@ -526,11 +526,6 @@ async function checkRounds(currentWordleIndex: number) {
   // }
 }
 async function missingUserGames(message: Message<boolean>) {
-  if (message.author.id === "305483990742073344") {
-    message.reply("no");
-    return;
-  }
-
   const wordleMeta = await WordleMeta.findOne();
 
   if (wordleMeta) {
